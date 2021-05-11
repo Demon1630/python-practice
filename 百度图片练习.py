9f0641b92805c15ce0a4a777739bfa453b97f479
demon <30583788+Demon1630@users.noreply.github.com>
2021-05-11 23:01:51 +0800

    添加 下载页面数，自定义下载多少页图片，每页30张

--- 百度图片练习.py
+++ 百度图片练习.py
@@ -10,7 +10,7 @@ headers = {
 }
 
 keyword = input('请输入搜索关键字：')
-print(keyword)
+# print(keyword)
 
 dir_name = ''.join(random.sample(string.ascii_letters+string.digits,8))
 
@@ -19,14 +19,18 @@ os.mkdir('C:\\Users\\Administrator\\Desktop\\'+dir_name)
 print(f'图片将存储在：{dir_name}文件夹中')
 
 
-max_value = 150
-current_value = 0
+current_value = 1
 
 pn = 0
 
 
-url = 'https://image.baidu.com/search/acjson?'
-param = {
+page = int(input('输入爬取页面数：'))+1
+
+image_index = 1
+for m in range(1,page):
+
+    url = 'https://image.baidu.com/search/acjson?'
+    param = {
         'tn': 'resultjson_com',
         'logid': ' 7517080705015306512',
         'ipn': 'rj',
@@ -64,39 +68,29 @@ param = {
         'gsm': '1e',
     }
 
-
-
-image_index = 1
-while current_value < max_value:
-
     # result = requests.get('https://image.baidu.com/search/acjson?tn=resultjson_com&logid=11092943101252335937&ipn=rj&ct=201326592&is=&fp=result&queryWord=%E5%A4%96%E6%98%9F%E4%BA%BA&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=0&hd=&latest=&copyright=&word={}s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&pn={}&rn=30&gsm=1e&1620734157038='.format(word,current_value),headers=headers)
     result = requests.get(url=url,headers=headers,params=param)
-    # print(result)
     result.encoding = 'utf-8'
     result = result.text
-    # json_str = result.content
-    # json_doc = str(json_str,'utf-8')
-    # print(json_doc)
 
     image_result = json.loads(result)
 
-    # print(image_result)
     data = image_result["data"]
-    print(data)
 
 
     for record in data:
         url = record.get('middleURL')
         if url != None:
-            print(f'正在下载图片：{url}')
+            print(f'正在下载图片{image_index}：{url}')
             r = requests.get(url=url,headers=headers)
             file_name = 'C:\\Users\\Administrator\\Desktop\\'+dir_name +'\\'+ str(image_index).zfill(10) + ".png"
 
             with open(file_name,'wb') as f:
                 f.write(r.content)
                 image_index += 1
-        current_value += 30
-    print('图像下载完成！')
+        # current_value += 29
+        pn += 29
+    print(f'页面{m}图像下载完成！')
 
 
 
