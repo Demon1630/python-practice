b42eef38aa45af91f92258eaa77ed49e302c96a4
demon <30583788+Demon1630@users.noreply.github.com>
2021-05-11 20:59:32 +0800

    异步爬取，但data无法提取

--- 爬取百度图片练习.py
+++ 爬取百度图片练习.py
@@ -1,38 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
-
-
-# headers = {
-#     'Accept':' text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-#     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-# }
+import json
+import random
+import string
+import os
 
 headers = {
     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
 }
 
+word = input('请输入搜索关键字：')
+print(word)
+
+dir_name = input('请输入存储文件夹名：')
+os.mkdir('C:\\Users\\Administrator\\Desktop\\'+dir_name)
+print(f'图片将存储在：{dir_name}文件夹中')
+
+
+max_value = 100
+current_value = 0
+
+image_index = 1
+while current_value < max_value:
+
+    result = requests.get('https://image.baidu.com/search/acjson?tn=resultjson_com&logid=11092943101252335937&ipn=rj&ct=201326592&is=&fp=result&queryWord=%E5%A4%96%E6%98%9F%E4%BA%BA&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=0&hd=&latest=&copyright=&word={}s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&pn={}&rn=30&gsm=1e&1620734157038='.format(word,current_value),headers=headers)
+
+    # print(result)
+
+    json_str = result.content
+    json_doc = str(json_str,'utf-8')
+    # print(json_doc)
+
+    image_result = json.loads(json_doc)
+
+    print(image_result)
+    data = image_result['data']
+    # print(data)
+
+    #
+    # for record in data:
+    #     url = record.get('middleURL')
+    #     if url != None:
+    #         print(f'正在下载图片：{url}')
+    #         r = requests.get(url=url,headers=headers)
+    #         file_name = dir_name + '/' + str(image_index).zfill(10) + ".png"
+    #
+    #         with open(file_name,'wb') as f :
+    #             f.write(r.content)
+    #             image_index += 1
+    #     current_value += 30
+    # print('图像下载完成！')
+    #
 
 
 
-url = 'https://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&fm=index&fr=&hs=0&xthttps=111110&sf=1&fmq=&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&word=%E7%BE%8E%E5%A5%B3&oq=%E7%BE%8E%E5%A5%B3&rsp=-1'
 
-r = requests.get(url=url,headers=headers)
-
-html = r.content
-
-soup = BeautifulSoup(html,'html.parser',from_encoding='utf-8')
-print(soup)
-
-f = open('C:\\Users\\Administrator\\Desktop\\1111.txt', 'a+',encoding='utf-8')
-f.write(soup.prettify())
-f.write('2')
-f.close()
-
-spans = soup.find_all(name='div',attrs={'class':'imgbox'})
-# spans = soup.find_all(name='img',attrs={'class':'main_img img-hover'})
-
-# print(soup.title)
-# for i in  spans:
-#     print(i)
-# print(spans)
