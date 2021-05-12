9308a28d9c20154b6506eb0fba5c02463cf7bfac
demon <30583788+Demon1630@users.noreply.github.com>
2021-05-12 22:25:38 +0800

    如何使用beautifulsoup解析html中内容

--- beautifulsoup4_prectice3.py
+++ beautifulsoup4_prectice3.py
@@ -41,74 +41,100 @@ html = """
 </html>
 """
 
+
+
+
+
 #1.得到beautifulsoup对象
 soup = BeautifulSoup(html,'html.parser')
-#2.获取内容
-#获取标题对象
-print(soup.title)
-#获取文本内容
-print(soup.title.get_text())
-print(soup.title.string)
-print(soup.title.text)
-
-#通过上下级关系获取
-print(soup.title.parent)   #父级
-print(soup.title.child)#没有下级返回None
-
-#获取第一个P标签
-print(soup.p)
-#获取P的孩子们
-print(soup.p.children)
-for i,each in enumerate(soup.p.children):
-    print(i,each)
-print(list(soup.p.children)[2])
 
 
-#获取标签的属性
-print(soup.a)
-print(soup.a.id)#获取不到
-print(soup.a.href)#获取不到
-print(soup.a.name)
-#应该这样写
-print(soup.a.attrs['id'])
-print(soup.a.attrs['href'])
-print(soup.a.attrs['class'])#class得到一个list
+# print(soup.p.attrs)
+# print(soup.div.string)
 
-#获取多个
-print(soup.find('p'))#获取一个P
-print(soup.find_all('p'))#获取SOUP内的P，要看是谁的find_all()
+# for i in soup.find_all('p'):
+#     print(i.attrs)
+#     print(i.attrs['class'])
 
-#多层查询
-print(soup.find_all('ul')[0].find_all('li'))
-print(soup.find('ul').find_all('li'))
-
-#通过指定的 属性获取对象
-print(soup.find(id='ulone'))#单个对象
-print(soup.find('ul',id='ulone'))#单个对象
-print(soup.find_all('ul',id='ulone'))#这个是列表，实际使用要加下表的
-
-#calss这么写class_
-print(soup.find_all('p',class_='title'))
-#更通用的方式
-print(soup.find_all('p',attrs={'class':'title'}))
-print(soup.find_all('p',attrs={'class':'title','title':'new'}))
-
-#使用函数 作为参数获取元素
-def judgeTitle(t):
-    if t == 'div11':
-        return True
-print(soup.find_all(class_=judgeTitle))
-
-import re
-reg = re.compile('sis')
-def judgeLen(t):
-    #f返回长度为6，且包含’sis‘的参数
-    return len(str(t))==6 and bool(re.search(reg,t))
-print(soup.find_all(class_=judgeLen))
+#奇怪
+print(soup.find_all(name='dromouse'))   #为什么name属性不行
+# print(soup.find_all(title="new"))
+print(soup.find_all(class_="title"))
+print(soup.find_all(class_="title",title="new"))   #多个属性过滤
 
 
-#limit参数：限制访问个数
-print(soup.find_all('a',limit=2))
+# for i in soup.p.children:
+#     print(i)
 
-#recursive参数：recursive = Ture 寻找子孙。false只寻找子
-print(soup.find_all('body')[0].find_all('ul',recursive='false'))
+
+# #2.获取内容
+# #获取标题对象
+# print(soup.title)
+# #获取文本内容
+# print(soup.title.get_text())
+# print(soup.title.string)
+# print(soup.title.text)
+#
+#
+#
+# #通过上下级关系获取
+# print(soup.title.parent)   #父级
+# print(soup.title.child)#没有下级返回None
+#
+# #获取第一个P标签
+# print(soup.p)
+# #获取P的孩子们
+# print(soup.p.children)
+# for i,each in enumerate(soup.p.children):
+#     print(i,each)
+# print(list(soup.p.children)[2])
+#
+#
+# #获取标签的属性
+# print(soup.a)
+# print(soup.a.id)#获取不到
+# print(soup.a.href)#获取不到
+# print(soup.a.name)
+# #应该这样写
+# print(soup.a.attrs['id'])
+# print(soup.a.attrs['href'])
+# print(soup.a.attrs['class'])#class得到一个list
+#
+# #获取多个
+# print(soup.find('p'))#获取一个P
+# print(soup.find_all('p'))#获取SOUP内的P，要看是谁的find_all()
+#
+# #多层查询
+# print(soup.find_all('ul')[0].find_all('li'))
+# print(soup.find('ul').find_all('li'))
+#
+# #通过指定的 属性获取对象
+# print(soup.find(id='ulone'))#单个对象
+# print(soup.find('ul',id='ulone'))#单个对象
+# print(soup.find_all('ul',id='ulone'))#这个是列表，实际使用要加下表的
+#
+# #calss这么写class_
+# print(soup.find_all('p',class_='title'))
+# #更通用的方式
+# print(soup.find_all('p',attrs={'class':'title'}))
+# print(soup.find_all('p',attrs={'class':'title','title':'new'}))
+#
+# #使用函数 作为参数获取元素
+# def judgeTitle(t):
+#     if t == 'div11':
+#         return True
+# print(soup.find_all(class_=judgeTitle))
+#
+# import re
+# reg = re.compile('sis')
+# def judgeLen(t):
+#     #f返回长度为6，且包含’sis‘的参数
+#     return len(str(t))==6 and bool(re.search(reg,t))
+# print(soup.find_all(class_=judgeLen))
+#
+#
+# #limit参数：限制访问个数
+# print(soup.find_all('a',limit=2))
+#
+# #recursive参数：recursive = Ture 寻找子孙。false只寻找子
+# print(soup.find_all('body')[0].find_all('ul',recursive='false'))
