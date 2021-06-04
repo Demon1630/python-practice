eaa7588c2fc993c9a69c564dd1c983da7dcb125e
demon <30583788+Demon1630@users.noreply.github.com>
2021-06-04 11:12:47 +0800

    使用企业微信推送服务

---
+++ server jiang test.py
@@ -0,0 +1,16 @@
+import requests
+from  fake_useragent import UserAgent
+
+def send_wechat(text):
+    # url = 'https://sc.ftqq.com/SCU93555Tc0608f46612b3f58458bc7236a6b17285e91f9c6173ab.send'
+    url = 'https://sctapi.ftqq.com/SCT43501TagBvqSsCzQ5zc1ZyBbthfB8L.send'
+    headers = {
+            'User-Agent':UserAgent().random
+    }
+    key = {
+        'title':'通知',
+        'desp':text,
+    }
+    response = requests.post(url=url,headers=headers,data=key)
+    # print(response)
+
