a4a1247894a756effdd4b60610b6cdd219b59967
demon <30583788+Demon1630@users.noreply.github.com>
2021-06-07 08:59:48 +0800

    改良了cookie获取办法，但还是不行

--- 易校园登入.py
+++ 易校园登入.py
@@ -10,90 +10,86 @@ import telegram
 import schedule
 
 
-# def get_in():
-#     url = 'https://application.xiaofubao.com/app/invite/jssdk'
-#
-#     headers = {
-#         'Host': 'application.xiaofubao.com',
-#         'Connection': 'keep-alive',
-#         'Content-Length': '65',
-#         'deviceId':'',
-#         'User-Agent':'Mozilla/5.0 (Linux; Android 10; MI 8 Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.210 Mobile Safari/537.36 Html5Plus/1.0 (Immersed/32.363636) ZJYXYwebviewbroswer ZJYXYAndroid tourCustomer /yunmaapp.NET/2.1.2/0c5bf2652e3c331c',
-#         'Content-Type':'application/x-www-form-urlencoded;charset=UTF-8',
-#         'OSVersion':'',
-#         'timestamp':'',
-#         'userId':'',
-#         'signature':'',
-#         'appId':'',
-#         'token':'',
-#         'Accept': '*/*',
-#         'Origin': 'https://application.xiaofubao.com',
-#         'X-Requested-With': 'cn.com.yunma.school.app',
-#         'Sec-Fetch-Site': 'same-origin',
-#         'Sec-Fetch-Mode': 'cors',
-#         'Sec-Fetch-Dest': 'empty',
-#         'Referer': 'https://application.xiaofubao.com/',
-#         'Accept-Encoding': 'gzip, deflate',
-#         'Accept-Language':'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-#         'Cookie': 'shiroJID=7cd3aaa3-9b6b-4d7d-83e9-ff0a92ae361a',
-#
-#         'Set-Cookie': 'shiroJID=7cd3aaa3-9b6b-4d7d-83e9-ff0a92ae361a',
-#         'Path' : '/',
-#         'HttpOnly':'',
-#         'SameSite' : 'lax',
-#
-#     }
-#
-#     key = {
-#         'url':'https%3A%2F%2Fapplication.xiaofubao.com%2F',
-#         'platform':'YUNMA_APP',
-#     }
-#
-#     respons = requests.post(url=url,headers=headers,data=key)
-#
-#     print(respons)
-#     print(respons.text)
 
 
-def get_in():
-    url = 'https://compus.xiaofubao.com/login/doLoginBySilent'
+def get_cookie():
+
+
+    f = open(r'C:\Users\Administrator\Desktop\cookie.txt', 'r+')  # 从cookie。txt文件中读取原cookie
+    # old_cookie='shiroJID=44c8aa33-bf1d-4478-b224-74e84c52b16a'
+    # old_cookie='shiroJID=ed0b475d-52f3-4af9-a2f5-35dc54dcf9a9'
+    old_cookie = f.read()
+    # print(old_cookie)
+    f.close()
+
+
+
+    # url = 'https://application.xiaofubao.com/app/invite/jssdk'
+    url = 'https://application.xiaofubao.com/app/login/getUser4Authorize'
+
+
 
     headers = {
-        'Accept':'application/json',
-        'User-Agent':'Mozilla/5.0 (Linux; Android 10; MI 8 Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.210 Mobile Safari/537.36 Html5Plus/1.0 (Immersed/32.363636) ZJYXYwebviewbroswer ZJYXYAndroid tourCustomer /yunmaapp.NET/2.1.2/0c5bf2652e3c331c',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Host': 'compus.xiaofubao.com',
-        'Connection': 'Keep-Alive',
-        'Accept-Encoding': 'gzip',
-        'Content-Length': '297',
-
-        'Set-Cookie': 'shiroJID=7cd3aaa3-9b6b-4d7d-83e9-ff0a92ae361a',
-        'Path' : '/',
-        'HttpOnly':'',
-        'SameSite' : 'lax',
+        'Host': 'application.xiaofubao.com',
+        'Connection': 'keep-alive',
+        'Content-Length': '76',
+        'deviceId':'',
+        # 'User-Agent':'Mozilla/5.0 (Linux; Android 10; MI 8 Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.210 Mobile Safari/537.36 Html5Plus/1.0 (Immersed/32.363636) ZJYXYwebviewbroswer ZJYXYAndroid tourCustomer /yunmaapp.NET/2.1.2/0c5bf2652e3c331c',
+        'Content-Type':'application/x-www-form-urlencoded;charset=UTF-8',
+        'OSVersion':'',
+        'timestamp':'',
+        'userId':'',
+        'signature':'',
+        'appId':'',
+        'token':'',
+        'Accept': '*/*',
+        'Origin': 'https://application.xiaofubao.com',
+        'X-Requested-With': 'cn.com.yunma.school.app',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://application.xiaofubao.com/',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language':'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        # 'Cookie': old_cookie,  #cookie 没过期时不会返回新cookie
 
     }
 
     key = {
-        'clientId' : '3a2c98574679a76d4fbb1c3cd4bc4f73',
-        'osType' : 'Android',
-        'osVersion' : '10',
-        'mobileType' : 'MI+8',
-        'oaid' : '0c5bf2652e3c331c',
-        'appAllVersion' : '2.1.2',
-        'appWgtVersion' : '2.1.3',
-        'id' : '1906280823241710',
+        # 'url':'https%3A%2F%2Fapplication.xiaofubao.com%2F',
+        # 'platform':'YUNMA_APP',
+        'code' : 'a2309ba493fdf938ec0617f28571496b',
+        'userId' : '1906280823241710',
         'schoolCode' : '19335',
-        'token' : 'b4d44185afbb47bc9d3038c406bb9331',
-        'deviceId' : '0c5bf2652e3c331c',
-        'testAccount' : '1',
-        'appVersion' : '170',
         'platform' : 'YUNMA_APP',
     }
+
     respons = requests.post(url=url,headers=headers,data=key)
 
-    print(respons)
-    print(respons.text)
+    # print(respons)
+    # print(respons.text)
+    # print(respons.headers)
+    # print(old_cookie)
+    if str(respons.headers).find('Set-Cookie') != -1:
+        new_header = eval(str(respons.headers))
+        # for key,value in new_header.items():
+        #     print(f'{key}={value}')
+        new_Cookie = new_header['Set-Cookie'].split(';',2)
+        print(new_Cookie[0])
+
+        f = open(r'C:\Users\Administrator\Desktop\cookie.txt', 'r+')   # 将获取到的新cookie存入cookie.txt文件
+        f.write(str(new_Cookie[0]))
+        f.close()
+
+        return new_Cookie[0]
+    else:
+        # print(old_cookie)
+        return old_cookie
+
+    # print(type(eval(str(respons.headers))))
 
 
-get_in()
+
+new_cookie = get_cookie()
+
+print(new_cookie)
