2d98bc1edd5b372c434ec71f15d0a9d55c2c3f64
demon <30583788+Demon1630@users.noreply.github.com>
2021-06-14 17:58:38 +0800

    使用自己编写的企业微信接口发送消息

---
+++ 使用企业微信发送消息.py
@@ -0,0 +1,54 @@
+# -*- coding: UTF-8 ...
+import re
+import requests
+from bs4 import BeautifulSoup
+from  fake_useragent import UserAgent
+import os
+import telegram
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+import time
+
+
+def send_wechat(title,text,detal_url):
+
+    get_acs_token_url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=ww428187fbdb9fd50f&corpsecret=po3lAng2YdZzaa67teFhmOxL34EuuWpffdiZlQeGaNk'
+
+    access = requests.get(url=get_acs_token_url)
+    access_token = eval(access.text)['access_token']
+    # print(type(eval(access_token.text)))
+    # print(access_token)
+
+
+    url = f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}'
+    print(url)
+
+    headers = {
+        'User-Agent': UserAgent().random
+    }
+
+    key = {
+        "touser": "@all",
+        # "toparty": "PartyID1|PartyID2",
+        # "totag": "TagID1 | TagID2",
+        "msgtype": "textcard",
+        "agentid": 1000002,
+        # "text": '测试',
+        "textcard": {
+            "title": title,
+            "description": text,#"<div class=\"gray\">2016年9月26日</div> <div class=\"normal\">恭喜你抽中iPhone 7一台，领奖码：xxxx</div><div class=\"highlight\">请于2016年10月10日前联系行政同事领取</div>",
+            "url": detal_url,
+            "btntxt": "更多"
+        },
+        "safe": 0,
+        "enable_id_trans": 0,
+        "enable_duplicate_check": 0,
+        "duplicate_check_interval": 1800
+    }
+
+    res = requests.post(url=url,headers=headers,json=key)
+    print(res.status_code)
+    print(res.text)
+
+send_wechat('测试','你好啊','URL')
