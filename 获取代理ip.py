9b21e2654efbd0a848d2ca9730b6de87c5da4751
demon <30583788+Demon1630@users.noreply.github.com>
2021-06-16 09:54:16 +0800

    增加爬取完成后提醒

--- 获取代理ip.py
+++ 获取代理ip.py
@@ -179,6 +179,56 @@ def delate_ip(ip):
         print(f'{ip}无效，删除')
 
 
+def send_wechat(title,text,detal_url):
+
+    get_acs_token_url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=ww428187fbdb9fd50f&corpsecret=po3lAng2YdZzaa67teFhmOxL34EuuWpffdiZlQeGaNk'
+
+    access = requests.get(url=get_acs_token_url)
+    access_token = eval(access.text)['access_token']
+    # print(type(eval(access_token.text)))
+    # print(access_token)
+
+
+    url = f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}'
+    print(url)
+
+    headers = {
+        'User-Agent': UserAgent().random
+    }
+
+    key = {
+        "touser": "@all",
+        # "toparty": "PartyID1|PartyID2",
+        # "totag": "TagID1 | TagID2",
+        "msgtype": "textcard",
+        "agentid": 1000002,
+        # "text": '测试',
+        "textcard": {
+            "title": title,
+            "description": text,#"<div class=\"gray\">2016年9月26日</div> <div class=\"normal\">恭喜你抽中iPhone 7一台，领奖码：xxxx</div><div class=\"highlight\">请于2016年10月10日前联系行政同事领取</div>",
+            "url": detal_url,
+            "btntxt": "更多"
+        },
+        "safe": 0,
+        "enable_id_trans": 0,
+        "enable_duplicate_check": 0,
+        "duplicate_check_interval": 1800
+    }
+
+    res = requests.post(url=url,headers=headers,json=key)
+    # print(res.status_code)
+    # print(res.text)
+
+# send_wechat('测试','你好啊','URL')
+
+
+def send_telegram(text):
+    chat_id = '1203976293'
+    token = '1805409515:AAEQDXHNpZkgRe27udY99D5Rv3q9cNhDpyI'
+    bot = telegram.Bot(token=token)
+
+    bot.send_message(chat_id=chat_id, text=text)
+
 def main():
 
     new_ip = get_excel_ip()
@@ -227,6 +277,9 @@ def main():
                 j=0
             j+=1
             time.sleep(5)
+
+    send_wechat('IP池爬取', '爬取完成', 'URL')
+    send_telegram('IP池爬取完成')
 
     #从exel中获取一个有效ip并返回
     # ip_useful = get_excel()
